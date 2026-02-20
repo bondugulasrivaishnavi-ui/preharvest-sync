5fbb3ae6e11ef9dbc6b63bb93e6a4ddb6cd7eb3f
AshmithaReddy-23 <24071a12c9@vnrvjiet.in>
2026-02-20 19:02:00 +0530

    Update DashboardLayout.jsx

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,1 +1,21 @@
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
 
+function DashboardLayout() {
+  return (
+    <div className="flex h-screen bg-gray-100">
+      <Sidebar />
+
+      <div className="flex-1 flex flex-col">
+        <Navbar />
+
+        <main className="p-6 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardLayout;
