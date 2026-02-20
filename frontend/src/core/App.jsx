54c300c7460ef0909ab0ab2459450b80e9325158
AshmithaReddy-23 <24071a12c9@vnrvjiet.in>
2026-02-20 18:58:41 +0530

    Update App.jsx

--- frontend/src/core/App.jsx
+++ frontend/src/core/App.jsx
@@ -1,1 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DashboardLayout from "../layouts/DashboardLayout";
+import FarmerDashboard from "../modules/farmer/FarmerDashboard";
+import BuyerDashboard from "../modules/buyer/BuyerDashboard";
+import TransportDashboard from "../modules/transport/TransportDashboard";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<DashboardLayout />}>
+          <Route path="/" element={<FarmerDashboard />} />
+          <Route path="/buyer" element={<BuyerDashboard />} />
+          <Route path="/transport" element={<TransportDashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
